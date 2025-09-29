bf1d85b5b53d12afba8d9130e5a531d604581b3e
qetiproject <ketideveloper@gmail.com>
2025-09-30 00:21:53 +0400

    guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { UserPermission } from './core/guards/user-permission';
 import { ProductDetailComponent } from './products-module/pages/product-detail/product-detail.component';
 import { ProductDetailsResolver } from './products-module/pages/product-detail/product-details.resolver';
 import { ProductListComponent } from './products-module/pages/product-list/product-list.component';
@@ -13,7 +14,12 @@ export const routes: Routes = [
     path: 'products',
     children: [
       { path: '', component: ProductListComponent },
-      { path: ':id', component: ProductDetailComponent, resolve: { productDetails: ProductDetailsResolver} },
+      { 
+        path: ':id', 
+        component: ProductDetailComponent, 
+        resolve: { productDetails: ProductDetailsResolver},
+        canActivate: [UserPermission],
+      },
     ],
   },
   {
