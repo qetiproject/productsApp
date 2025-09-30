2ac399364970ff160ab378795ac4cd75ba4a2a9d
qetiproject <ketideveloper@gmail.com>
2025-09-30 08:01:50 +0400

    lazy loading

--- src/app/products-module/services/product.service.ts
+++ src/app/products-module/services/product.service.ts
@@ -9,32 +9,10 @@ import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
   providedIn: 'root'
 })
 export class ProductService {
-  // getProductAndThenReload(id: any) {
-  //   this.http.get<any[]>('/assets/data/products.json').subscribe(list => {
-  //     of(list).pipe(
-  //       switchMap(all =>
-  //         this.http.get<any[]>('/assets/data/products.json').pipe(
-  //           map(second => second.find(x => x.id == id))
-  //         )
-  //       )
-  //     ).subscribe(final => {
-
-  //       this.selectedProduct$.next(final);
-  //     });
-  //   });
-  // }
-
   #http = inject(HttpClient);
   #productsSubject = new BehaviorSubject<Product[]>([]);
   products$ = this.#productsSubject.asObservable();
 
-  // getProducts(): void {
-  //   if (!this.#productsSubject.value.length) {
-  //     this.#http.get<Product[]>(environment.baseUrl).pipe(
-  //       catchError(() => of([]))
-  //     ).subscribe(result => this.#productsSubject.next(result));
-  //   }
-  // }
   getProducts(): Observable<Product[]> {
     return  this.#http.get<Product[]>(environment.baseUrl);
   }
