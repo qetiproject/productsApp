ec02863c1bbd3cb96abe3d7a915533d2c93f1675
qetiproject <ketideveloper@gmail.com>
2025-09-30 09:14:33 +0400

    translate service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TranslateModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'CodeR';
+
+  constructor(private translate: TranslateService) {
+    this.translate.addLangs(['en', 'ka']);
+    const defaultLang = 'en';
+    this.translate.use(defaultLang);
+    this.translate.use('ka');
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,15 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
 import { provideEffects } from '@ngrx/effects';
 import { provideStore } from '@ngrx/store';
 import { provideStoreDevtools } from "@ngrx/store-devtools";
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { productReducer } from '@products-module/store/product.reducer';
 import { routes } from './app.routes';
 import { ProductEffect } from './products-module/store/product.effect';
+import { customLoaderFactory } from './utils/custom-translater';
 
 export const appConfig: ApplicationConfig = {
    providers: [
@@ -24,5 +26,14 @@ export const appConfig: ApplicationConfig = {
       maxAge: 25,
       logOnly: false
     }),
+    importProvidersFrom([
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: customLoaderFactory,
+          deps: [HttpClient],
+        },
+      }),
+    ]),
   ]
 };
